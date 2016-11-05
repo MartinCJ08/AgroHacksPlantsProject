43651e475cb9d6c4219c26e0489b0c1f0a2b1bbe
bestjonav2 <jona622@hotmail.com>
2016-11-05 10:47:00 -0700

    test de querys

--- app/src/main/java/plants/chimichangas/agrohacks/com/agrohacksplantsproject/FructoActivity.java
+++ app/src/main/java/plants/chimichangas/agrohacks/com/agrohacksplantsproject/FructoActivity.java
@@ -1,13 +1,17 @@
 package plants.chimichangas.agrohacks.com.agrohacksplantsproject;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class FructoActivity extends AppCompatActivity {
+import plants.chimichangas.agrohacks.com.agrohacksplantsproject.database.DBHelper;
+import plants.chimichangas.agrohacks.com.agrohacksplantsproject.database.DBManager;
 
+public class FructoActivity extends AppCompatActivity {
+    private String sqlQuery="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,21 +20,24 @@ public class FructoActivity extends AppCompatActivity {
 
     public void onClickScroll(View view){
         Intent activity;
-        String sName;
-
+        String sName="";
+        sqlQuery = "SELECT name result FROM plants WHERE name=="+sName;
         switch (view.getId()) {
             case R.id.ibtnAlfa:
                 //DO something
                 System.out.println("Alfalfa");
-                sName = "Alfalfa";
-                activity = new Intent(getApplicationContext(), FrucScrollActivity.class);
-                startActivity(activity);
+                sName = "Cebolla";
+                DBHelper db = new DBHelper(this);
+                if(db.getNameQuery().equals(sName)){
+                    activity = new Intent(getApplicationContext(), FrucScrollActivity.class);
+                    startActivity(activity);
+                }
                 break;
             case R.id.ibtnCeb:
                 //DO something
 
                 System.out.println("Cebolla");
-                sName = "Alfalfa";
+                sName = "Cebolla";
                 activity = new Intent(getApplicationContext(), FrucScrollActivity.class);
                 startActivity(activity);
                 break;
@@ -54,5 +61,8 @@ public class FructoActivity extends AppCompatActivity {
                 startActivity(activity);
                 break;
         }
+    }
+    public String getSqlQuery(){
+        return sqlQuery;
     }
 }
--- app/src/main/java/plants/chimichangas/agrohacks/com/agrohacksplantsproject/database/DBManager.java
+++ app/src/main/java/plants/chimichangas/agrohacks/com/agrohacksplantsproject/database/DBManager.java
@@ -8,11 +8,9 @@ public class DBManager {
     public static final String TABLE_NAME="plants";
     public static final String ID="_id";
     public static final String NAME="name";
-    public static final String LOGO_URI = "logoUri";
     public static final String PLAGAS = "plagas";
     public static final String CONSEJOS = "consejos";
 
     public static final String CREATE_TABLE=" create table "+TABLE_NAME+" ("+ID
-                    +" integer primary key autoincrement not null,"+NAME+" text not null,"+LOGO_URI+" text not null,"+
-                    PLAGAS+" text not null,"+CONSEJOS+" text not null);";
+                    +" integer primary key autoincrement not null,"+NAME+" text not null,"+PLAGAS+" text not null,"+CONSEJOS+" text not null);";
 }
