665d3ffe1b81a775abf696d6237fb3d130846194
MartinCJ08 <martin.carrascoj@gmail.com>
2016-11-05 02:05:06 -0600

    Avance activities

--- app/src/main/java/plants/chimichangas/agrohacks/com/agrohacksplantsproject/MainActivity.java
+++ app/src/main/java/plants/chimichangas/agrohacks/com/agrohacksplantsproject/MainActivity.java
@@ -12,8 +12,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
     public void onClick(View view){
-        Intent activity2 = new Intent(getApplicationContext(), Activity2.class);
-        startActivity(activity2);
+
+        //Intent activity2 = new Intent(getApplicationContext(), Activity2.class);
+        Intent activity;
+        //startActivity(activity2);
+        switch (view.getId()) {
+            case R.id.ibtnCultivo:
+                //DO something
+                System.out.println("Cultivo");
+                activity = new Intent(getApplicationContext(), FructoActivity.class);
+                startActivity(activity);
+
+                break;
+            case R.id.ibtnProbl:
+                //DO something
+                System.out.println("Problemas");
+                break;
+
+        }
     }
 }
