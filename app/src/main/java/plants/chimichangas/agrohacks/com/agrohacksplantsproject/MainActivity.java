7c67872bb0556226e1bf41fcce9522ce9b162d26
bestjonav2 <jona622@hotmail.com>
2016-11-05 00:11:00 -0700

    Test

--- app/src/main/java/plants/chimichangas/agrohacks/com/agrohacksplantsproject/MainActivity.java
+++ app/src/main/java/plants/chimichangas/agrohacks/com/agrohacksplantsproject/MainActivity.java
@@ -1,7 +1,9 @@
 package plants.chimichangas.agrohacks.com.agrohacksplantsproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view){
+        Intent activity2 = new Intent(getApplicationContext(), Activity2.class);
+        startActivity(activity2);
     }
 }
